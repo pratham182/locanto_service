7019240c0ac7b10090ee69f153039fabe45bdcd6
pratham182 <60971558+pratham182@users.noreply.github.com>
2024-07-05 20:14:09 +0530

    Delete backend/dist directory

--- backend/dist/routes/auth.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { login, register } from "../controller/auth.js";
-const router = Router();
-router.post("/register", register);
-router.post("/login", login);
-export default router;
--- backend/dist/utils/authUtil.js
+++
@@ -1,9 +0,0 @@
-import { compareSync, genSaltSync, hashSync } from "bcrypt-ts";
-export const hashPassword = (password) => {
-    const salt = genSaltSync(10);
-    const hash = hashSync(password, salt);
-    return hash;
-};
-export const comparePassword = ((hashPassword, originalPassword) => {
-    return compareSync(originalPassword, hashPassword);
-});
