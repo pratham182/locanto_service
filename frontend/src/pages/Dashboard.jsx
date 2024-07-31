af2a7e4a3d0238cb32b0519a2ca5f569752da303
kartikkhattar12 <khattarkartik3@gmail.com>
2024-07-31 15:22:03 +0530

    pushing dashboard page

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -3,6 +3,8 @@ import { ListingCards } from "../component/ListingCards";
 import { ListingDetail } from "../component/ListingDetail";
 
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLayerGroup } from '@fortawesome/free-solid-svg-icons';
 const Dashboard = () => {
 
     return (
@@ -32,6 +34,43 @@ const Dashboard = () => {
 
 
                        <div id="bottom-part">
+
+                        <div id="nav-options ">
+                            <div class="flex">
+                                <div>
+                                    <button data-dropdown-toggle="dropdown">
+                                                                                  <FontAwesomeIcon  icon={faLayerGroup} />
+
+
+                                    </button>
+                                          <h3>
+                                            Listings
+                                          </h3>
+
+                                </div>
+
+
+                                <div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+    <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+      <li>
+        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+      </li>
+      <li>
+        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
+      </li>
+      <li>
+        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
+      </li>
+      <li>
+        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
+      </li>
+    </ul>
+</div>
+
+
+                            </div>
+                            
+                        </div>
 
                        </div>
                     </div>
@@ -90,3 +129,4 @@ const Dashboard = () => {
 }
 
 export default Dashboard;
+
